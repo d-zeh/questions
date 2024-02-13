f1d8b992a27abdc3050795fe0a7b7b05d7f6776e
Daniel Zeh <dfzeh1@gmail.com>
2024-02-12 22:03:02 -0600

    added questoin

---
+++ questions1.js
@@ -0,0 +1,62 @@
+// You are given a string s. Your task is to count the number of ways of splitting s into three non-empty parts a, b and c (s = a + b + c) in such a way that a + b, b + c and c + a are all different strings.
+
+// NOTE: + refers to string concatenation.
+
+// Example
+
+// For s = "xzxzx", the output should be solution(s) = 5.
+
+// Consider all the ways to split s into three non-empty parts:
+
+//     If a = "x", b = "z" and c = "xzx", then all a + b = "xz", b + c = "zxzx" and c + a = xzxx are different.
+//     If a = "x", b = "zx" and c = "zx", then all a + b = "xzx", b + c = "zxzx" and c + a = zxx are different.
+//     If a = "x", b = "zxz" and c = "x", then all a + b = "xzxz", b + c = "zxzx" and c + a = xx are different.
+//     If a = "xz", b = "x" and c = "zx", then a + b = b + c = "xzx". Hence, this split is not counted.
+//     If a = "xz", b = "xz" and c = "x", then all a + b = "xzxz", b + c = "xzx" and c + a = xxz are different.
+//     If a = "xzx", b = "z" and c = "x", then all a + b = "xzxz", b + c = "zx" and c + a = xxzx are different.
+
+// Since there are five valid ways to split s, the answer is 5.
+
+// Input/Output
+
+//     [execution time limit] 4 seconds (js)
+
+//     [memory limit] 1 GB
+
+//     [input] string s
+
+//     A string to split.
+
+//     Guaranteed constraints:
+//     3 ≤ s.length ≤ 100.
+
+//     [output] integer
+
+//     The number of ways to split the given string
+
+const { filter } = require("lodash");
+
+function solution(s) {
+    let solutions = [];
+    
+            function filterDuplicates(arr) {
+                return arr.filter((value, index, self) => self.indexOf(value) === index);
+            }
+
+    for(let i = 0; i < s.length; i++) {
+        for(let j = i+1; j < s.length; j++) {
+            let a = s.slice(0, i);
+            let b = s.slice(i, j);
+            let c = s.slice(j);
+            solutions.push(a+b);
+            solutions.push(b+c);
+            solutions.push(c+a);
+            
+        }
+        
+        
+        
+    }
+        filterDuplicates(solutions);
+        return filterDuplicates(solutions).length 
+}
